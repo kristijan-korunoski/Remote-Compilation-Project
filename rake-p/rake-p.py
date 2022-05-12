5d90f8d4a3f083c9a76963b9c1fe21af2c471783
kristijan-korunoski <93856849+kristijan-korunoski@users.noreply.github.com>
2022-05-12 13:10:09 +0800

    client and server transfer files
    Current rakefile will connect to two servers and complete actionset 1 on one server and actionset 2 on the other (because the cost increases on the first server as actions have been done)

--- rake-p/rake-p.py
+++ rake-p/rake-p.py
@@ -1,12 +1,17 @@
+from fileinput import filename
 import subprocess
 import socket
 import threading
+import os
 
 SIZE = 1024
 FORMAT = "utf-8"
 
 rakefile = open('Rakefile', 'r')
 content = rakefile.readlines()
+
+wd = os.getcwd()
+os.chdir(wd+"/required")
 
 all_lines = []
 default_port = 0
@@ -54,52 +59,133 @@ for host in hosts:
 print("Connections made:")
 for conn in s_array:
     print(conn.getpeername())
-print("\n")
 
 def send_to_server(server, send_data):
-    s_array[server].sendall(send_data.encode(FORMAT))   # should select one based on cost, also does this fragment data?
+    s_array[server].sendall(send_data.encode(FORMAT))
     ack=s_array[server].recv(1024).decode(FORMAT)
-    print(f"~ Recieved from server: {ack} ~\n")         # acks should contain more information
+    print(f"~ Recieved ack from server: {ack} ~\n")
     msg=s_array[server].recv(1024).decode(FORMAT)
     print(f"~ Recieved from server: {msg} ~\n")
+    if (msg == "\FileTransfer"):    # RECIVE FILE
+        s_array[server].sendall("ack_send_file".encode(FORMAT))
+        filename=s_array[server].recv(1024).decode(FORMAT)                 # Recieve filename
+        print(f"~ Recieved from server: {filename}\n")
+        s_array[server].sendall("client_recieved_filename".encode(FORMAT))   # Send acknowledgement back
+        file = open(f"{filename}", "wb")
+        numsends=s_array[server].recv(1024).decode(FORMAT)
+        s_array[server].sendall("client_recieved_loopnum".encode(FORMAT))   # Send acknowledgement back
+        print("Start copying file...\n")
+        #while True:
+        for i in range(int(numsends)+1):
+            filedata=s_array[server].recv(1024)#.decode(FORMAT)             # Recieve content
+            file.write(filedata)
+            #check = filedata.decode(FORMAT)
+            #if (check=="\EoF"):
+            #    conn.sendall("client_recieved_\EoF".encode(FORMAT))   # Send acknowledgement back
+            #    print(f"~ Recieved from server: {check}\n")
+            #    break
+        s_array[server].sendall("client_recieved_filedata".encode(FORMAT))   # Send acknowledgement back
+        file.close()
+        s_array[server].recv(1024).decode(FORMAT)
+
+def cost_query(server):
+    server = int(server)
+    s_array[server].sendall(("\CostQuery").encode(FORMAT))
+    cost=s_array[server].recv(1024).decode(FORMAT)   
+    print(f"~ Cost from server {server}: {cost} ~")
+    serverCosts[server] = int(cost)
+    updatedCosts[0] = updatedCosts[0] + 1
 
 # Start using lines at point 2 as 0 was for default port and 1 was for hosts
 n = 2
-while n < len(all_lines):
+numlines = len(all_lines)
+numhosts = len(hosts)
+print("Number of hosts:", numhosts, "\n")
+serverCosts = []
+optimalServer = -1
+for i in range(numhosts):
+    serverCosts.append(-1)
+
+while n < numlines:
     increment = 1
+    #print(f"/// {all_lines[n]} ///\n")
     if (all_lines[n][0][:9] == "actionset"):
         print(f"--- {all_lines[n][0]} ---\n")   # Request each server to start doing the cost query ???
+        updatedCosts = [0]
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        for i in range(numhosts):
+            new_thread = threading.Thread(target=cost_query,args=(str(i)))
+            new_thread.start()
+        while True:
+            if (updatedCosts[0] == numhosts):
+                break
+        minCost = min(serverCosts)
+        optimalServer = serverCosts.index(minCost)
+        print("\nLowest cost =", minCost)
+        print("Lowest cost server =", optimalServer)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
     if (all_lines[n][0][0] == "\t"):
         if (all_lines[n][0][1:8] == "remote-"): # Remote actions start with 'remote-' (after \t is discared)
             # Do remote action
             pass_var = all_lines[n][1:]
             pass_var.insert(0, all_lines[n][0][8:])
             # Check if next line has the required files, try block to handle indexing error when last line
-            try:
-                if (all_lines[n+1][0][1] == "\t"):
+            #try:
+            #print("number of lines = ", numlines)
+            #print("current line = ", n)
+            if n+1 < numlines:
+                if (all_lines[n+1][0] == "\t\trequires"):   # SEND FILE
+                    s_array[optimalServer].sendall(("\FileTransfer").encode(FORMAT))  # Send "\t\trequires" so server knows to recieve files
+                    ack=s_array[optimalServer].recv(1024).decode(FORMAT)   
+                    print(f"~ Recieved ack from server: {ack} ~\n")  
+                    numFiles = len(all_lines[n+1])-1                        # Calculate number of files to send
+                    s_array[optimalServer].sendall((str(numFiles)).encode(FORMAT))      # Send number of files to server
+                    ack=s_array[optimalServer].recv(1024).decode(FORMAT)
+                    print(f"~ Recieved ack from server: {ack} ~\n")
+                    while numFiles!=0:                                      # Start sending files
+                        fname = all_lines[n+1][numFiles]
+                        filesize = os.path.getsize(fname)
+                        s_array[optimalServer].sendall(fname.encode(FORMAT))    # Send filename
+                        ack=s_array[optimalServer].recv(1024).decode(FORMAT)
+                        print(f"~ Recieved ack from server: {ack} ~\n")
+                        print("FILESIZE IS _______ ", filesize, "\nFILENAME IS ________ ", fname, "\n")
+                        file = open(fname, 'rb') #open file
+                        content1 = file.read()
+                        numsends = int(filesize/1025)
+                        s_array[optimalServer].sendall(str(numsends).encode(FORMAT))       # Send number of times to recv
+                        ack=s_array[optimalServer].recv(1024).decode(FORMAT)
+                        print(f"~ Recieved ack from server: {ack} ~\n")
+                        s_array[optimalServer].sendall(content1)#.encode(FORMAT))                             # Send file content
+                        ack=s_array[optimalServer].recv(1024).decode(FORMAT)
+                        print(f"~ Recieved ack from server: {ack} ~\n")
+                        s_array[optimalServer].sendall("client_file_sent".encode(FORMAT))
+                        latest_file = fname
+                        numFiles-=1
                     increment = 2               # As next line is required for the current action the line increment is set to 2
-            except Exception as e:
-                increment = 1
-            # Send data to server
-            send_data = str(pass_var)           # later implement next line files
-            
-            for i in range(len(hosts)):         # Runs all remote actions on all servers
+            # Send data to server (action)
+            send_data = str(pass_var)
+            #for i in range(len(hosts)):         # Runs all remote actions on all servers
                                                 # it is suggested we use select() instead of threads here
-                new_thread = threading.Thread(target=send_to_server,args=(i, send_data))
-                new_thread.start()
+            send_to_server(optimalServer, send_data)
         else:
             # Format line so it can be passed to subprocess
             pass_var = all_lines[n][1:]
             pass_var.insert(0, all_lines[n][0][1:])
             # Check if next line has the required files, try block to handle indexing error when last line
-            try:
-                if (all_lines[n+1][0][1] == "\t"):
-                    increment = 2               # As next line is required for the current action the line increment is set to 2
-            except Exception as e:
-                increment = 1
+            #try:   Client compiling wont require sending to server
+            #    if (all_lines[n+1][0][1] == "\t"):
+            #        increment = 2               # As next line is required for the current action the line increment is set to 2
+            #except Exception as e:
+            #    increment = 1
             # We should first add this to an array of actions to be done by client and use threading to complete at once
             # Subprocess on client
             result = subprocess.run(pass_var)
             print(f"- Return code: {result.returncode} -\n")
     # By default move by one line
     n += increment
+for i in range(numhosts):
+    s_array[i].sendall("\EoR".encode(FORMAT))
+
+print("<<<<<<<<<<>>>>>>>>>>")
+print("<< Closing Client >>")
+print("<<<<<<<<<<>>>>>>>>>>")
--- rakeserver/rakeserver.py
+++ rakeserver/rakeserver.py
@@ -3,11 +3,19 @@ import shutil
 import socket
 import subprocess
 import time
+import glob
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 6283         # Port to listen on (non-privileged ports are > 1023)
+PORT = 6285         # Port to listen on (non-privileged ports are > 1023)
 SIZE = 1024
 FORMAT = "utf-8"
+
+shutil.rmtree("temp_s1")                       # Delete temp file and everything in it
+os.mkdir("temp_s1")                            # Create new temp file to store created files
+
+#ls = subprocess.run("ls")
+wd = os.getcwd()
+os.chdir(wd+"/temp_s1")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
@@ -16,26 +24,84 @@ s.listen()
 conn, addr = s.accept()
 print(f"New conection at {addr} \n")
 
-shutil.rmtree("temp")                       # Delete temp file and everything in it
-os.mkdir("temp")                            # Create new temp file to store created files
-
-r_actions = []                              # Store actions to do all at once
+cost = os.getpid()%3 + 1                    # Cost is initialized as random int
+latest_file = "\*ImpossibleFile*"
+#r_actions = []                              # Store actions to do all at once
 
 while True:
-    t = os.getpid()%5 + 2
-    #print(t)
-    time.sleep(t)
     data=conn.recv(1024).decode(FORMAT)     # Recieved as a string
-    conn.sendall("ack_s1".encode(FORMAT))   # Send acknowledgement back
-    if (data != ""):
+    if(data=="\EoR"):
+        print(f"Recieved from client: {data}\n")
+        print("<<<<<<<<<<>>>>>>>>>>")
+        print("<< Closing Server >>")
+        print("<<<<<<<<<<>>>>>>>>>>")
+        break
+    elif (data == "\CostQuery"):
+        print(f"Recieved from client: {data}\n")
+        print("Cost ===", cost, "\n")
+        conn.sendall((str(cost)).encode(FORMAT))   # Send acknowledgement back
+        #print(t)
+        #time.sleep(t)
+    elif (data =="\FileTransfer"):
+        print(f"Recieved from client: {data}\n")
+        conn.sendall("ack_s1_begin_file_transfer".encode(FORMAT))   # Send acknowledgement back
+        NoOfFiles = int(conn.recv(1024).decode(FORMAT))
+        print(f"Number of files to come from client:")
+        print(f"Recieved from client: {NoOfFiles}\n")
+        conn.sendall("ack_s1_recieved_number_of_files".encode(FORMAT))   # Send acknowledgement back
+        #print("Number of files:", NoOfFiles)
+        while(NoOfFiles!=0):        # RECIVE FILE
+            cost += 1 # Recieving a file increases cost for next time
+            
+            #conn.sendall("ack_send_file".encode(FORMAT))
+            filename=conn.recv(1024).decode(FORMAT)                 # Recieve filename
+            print(f"~ Recieved from client: {filename}\n")
+            print("Start copying file...\n")
+            conn.sendall("ack_s1_recieved_filename".encode(FORMAT))   # Send acknowledgement back
+            latest_file = filename
+            file = open(f"{filename}", "wb")
+            numsends=conn.recv(1024).decode(FORMAT)
+            conn.sendall("server_recieved_loopnum".encode(FORMAT))   # Send acknowledgement back
+            print("Start copying file...\n")
+            for i in range(int(numsends)+1):
+                filedata=conn.recv(1024)#.decode(FORMAT)             # Recieve content
+                file.write(filedata)
+            conn.sendall("client_recieved_filedata".encode(FORMAT))   # Send acknowledgement back
+            file.close()
+            conn.recv(1024).decode(FORMAT)
+            NoOfFiles-=1
+    elif (data != ""):
+        conn.sendall("ack_s1_recieved_action".encode(FORMAT))   # Send acknowledgement back
+        cost += 1 # Completing a command increases cost for next time
         pass_var = eval(data)
-        print(f"~ {pass_var} ~\n")
+        print(f"Recieved from client: {pass_var}\n")
         #r_actions.append(pass_var)
         result = subprocess.run(pass_var, stdout=subprocess.PIPE)   # Do processes as they come through (should just do whole actionset)
         send_back = str((result.stdout, result.returncode))         # Should write output to temp directory or something (see lecture on May 4th)         
-        conn.sendall(send_back.encode(FORMAT))                      # Send back stdout of result & returncode
-    if not data:
-        break
+        # If new file send back
+        currDir = os.getcwd()
+        list_of_files = glob.glob("*") # * means all if need specific format then *.csv
+        newest_file = max(list_of_files, key=os.path.getctime)
+        #print(newest_file)
+        if ((newest_file != latest_file)):      # SEND FILE
+            conn.sendall("\FileTransfer".encode(FORMAT))
+            conn.recv(1024).decode(FORMAT)              # Request acknowledged start sending file
+            conn.sendall(newest_file.encode(FORMAT))    # Send filename
+            conn.recv(1024).decode(FORMAT)
+            filesize = os.path.getsize(newest_file)
+            print("FILESIZE IS _______ ", filesize, "\nFILENAME IS ________ ", newest_file, "\n")
+            file = open(newest_file, 'rb') #open file
+            content1 = file.read()
+            numsends = int(filesize/1025)
+            conn.sendall(str(numsends).encode(FORMAT))       # Send file content
+            ack=conn.recv(1024).decode(FORMAT)
+            conn.sendall(content1)#.encode(FORMAT))       # Send file content
+            ack=conn.recv(1024).decode(FORMAT)
+            print(ack)
+            conn.sendall("ack_s1_file_sent".encode(FORMAT))
+            latest_file = newest_file
+        else:
+            conn.sendall(send_back.encode(FORMAT))                      # Send back stdout of result & returncode
     
 # Can be used to do all actions at one time with threads
 #print(f"Actions: {r_actions}")
